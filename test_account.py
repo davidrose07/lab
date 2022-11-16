5cd838e0bcf2c6f93db8b4c3d5fccc48d74679e9
david rose <davidrose071783@gmail.com>
2022-11-16 17:20:22 -0600

    Fixed Code

--- test_account.py
+++ test_account.py
@@ -1,6 +1,9 @@
 from account import Account
+from pytest import *
 
-class Test:    
+class Test: 
+    abs = .001
+       
     def setup_method(self):
         self.p1 = Account('Jane')
         self.p2 = Account('John')
@@ -15,21 +18,26 @@ class Test:
         assert self.p2.get_name() == 'John'
         assert self.p2.get_balance() == 0
         
-    
     def test_deposit(self):    
         assert self.p1.deposit("") == False
         assert self.p1.deposit("str") == False
         assert self.p1.deposit(-1) == False
+        assert self.p1.get_balance() == 0
         assert self.p1.deposit(0) == False
         assert self.p1.deposit(1.0) == True
+        assert self.p1.get_balance() == approx(1.0, abs=abs)
         assert self.p1.deposit(1) == True
+        assert self.p1.get_balance() == approx(2.0, abs=abs)
         
         assert self.p2.deposit("") == False
         assert self.p2.deposit("str") == False
         assert self.p2.deposit(-1) == False
+        assert self.p2.get_balance() == 0
         assert self.p2.deposit(0) == False
         assert self.p2.deposit(1.0) == True
+        assert self.p2.get_balance() == approx(1.0, abs=abs)
         assert self.p2.deposit(1) == True
+        assert self.p2.get_balance() == approx(2.0, abs=abs)
                  
     def test_withdrawl(self):
         assert self.p1.withdrawl("") == False
@@ -41,6 +49,8 @@ class Test:
         assert self.p1.deposit(10)
         assert self.p1.get_balance() == 10
         assert self.p1.withdrawl(11) == False
+        assert self.p1.withdrawl(9.5) is True
+        assert self.p1.get_balance() == approx(0.5, abs=abs)
         
         assert self.p2.withdrawl("") == False
         assert self.p2.withdrawl("str") == False
@@ -49,6 +59,8 @@ class Test:
         assert self.p2.get_balance() == 0
         assert self.p2.withdrawl(1) == False
         assert self.p2.deposit(10)
-        assert self.p1.get_balance() == 10
+        assert self.p2.get_balance() == 10
         assert self.p2.withdrawl(11) == False
+        assert self.p2.withdrawl(9.5) is True
+        assert self.p2.get_balance() == approx(0.5, abs=abs)
      
